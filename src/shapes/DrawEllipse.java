865cc71fc849e33388ab4cb65cc89a7a5ba91480
kevin endres <k.m.endres@gmail.com>
2021-04-26 16:40:04 -0500

    Added DrawEllipse functionality

--- src/shapes/DrawEllipse.java
+++ src/shapes/DrawEllipse.java
@@ -1,8 +1,10 @@
 package shapes;
 
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D.Float;
 import model.Point;
 import model.ShapeColor;
-import model.ShapeShadingType;
 import view.interfaces.PaintCanvasBase;
 
 public class DrawEllipse implements IDraw {
@@ -10,6 +12,8 @@ public class DrawEllipse implements IDraw {
   @Override
   public void draw(PaintCanvasBase paintCanvas, Point origin, int width, int height,
       ShapeColor primaryColor, ShapeColor secondaryColor, IShadingTypeStrategy shadingStrategy) {
-
+    Graphics2D g2d = paintCanvas.getGraphics2D();
+    Shape ellipse = new Float(origin.x, origin.y, width, height);
+    shadingStrategy.shade(g2d, ellipse, primaryColor.getColor(), secondaryColor.getColor());
   }
 }
